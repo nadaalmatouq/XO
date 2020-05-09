8fd3a6b2da65c1ca0c1736147402daa925c7b2c0
nada <64605593+nadaalmatouq@users.noreply.github.com>
2020-05-09 17:35:33 +0300

    Initial Commit

---
+++ XO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XO
+//
+//  Created by Nada Alm on 5/9/20.
+//  Copyright © 2020 Nada Alm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

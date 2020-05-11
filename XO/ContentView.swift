558eb9ee91f91d6f7009d577a610ed8a141c41e8
nada <64605593+nadaalmatouq@users.noreply.github.com>
2020-05-11 05:40:44 +0300

    complete using environmentObject

--- XO/ContentView.swift
+++ XO/ContentView.swift
@@ -9,20 +9,13 @@
 import SwiftUI
 
 
-struct Field{
+class Env: ObservableObject{
     
-    var text: String
-    var enabled: Bool
+    @Published var fields : [[Field]] = .init(repeating: .init(repeating:Field(text: "", enabled: true), count: 3), count: 3)
+    @Published var currentPlayer: String = "X"
+    @Published var winner: String = ""
+    @Published var counter: Int = 0
     
-}
-
-
-struct ContentView: View {
-    
-    @State var fields : [[Field]] = .init(repeating: .init(repeating:Field(text: "", enabled: true), count: 3), count: 3)
-    @State var currentPlayer: String = "X"
-    @State var winner: String = ""
-    @State var counter: Int = 0
     
     
     func restartGame() {
@@ -38,6 +31,72 @@ struct ContentView: View {
     
     
     
+    
+    func checkWinners(){
+               
+               
+               func checkWinner(p: String){
+               
+               let r1 = fields[0][0].text == p && fields[0][1].text == p && fields[0][2].text == p
+               let r2 = fields[1][0].text == p && fields[1][1].text == p && fields[1][2].text == p
+               let r3 = fields[2][0].text == p && fields[2][1].text == p && fields[2][2].text == p
+               
+               
+               let c1 = fields[0][0].text == p && fields[1][0].text == p && fields[2][0].text == p
+               let c2 = fields[0][1].text == p && fields[1][1].text == p && fields[2][1].text == p
+               let c3 = fields[0][2].text == p && fields[1][2].text == p && fields[2][2].text == p
+                   
+               let d1 = fields[0][0].text == p && fields[1][1].text == p && fields[2][2].text == p
+               let d2 = fields[0][2].text == p && fields[1][1].text == p && fields[2][0].text == p
+               
+               
+               if r1 || r2 || r3 || c1 || c2 || c3 || d1 || d2{
+                   
+                   winner = "\(p) wins!!"
+                   
+                   
+               }
+               else if counter == 9 && winner == ""{
+                   
+                   winner = "Tie!!"
+                   
+                   }
+                   
+               }//checkWinner
+               checkWinner(p: "X")
+               checkWinner(p: "O")
+               
+               
+           }//checkWinners
+    
+    
+    func buttonAction(r: Int,c: Int){
+        
+        if fields[r][c].enabled{
+                       fields[r][c].text = currentPlayer
+                       currentPlayer = currentPlayer == "X" ? "O" : "X"
+                       fields[r][c].enabled = false
+                       
+                       counter += 1
+            checkWinners()
+    }
+    
+    }//end func buttonAction
+    
+}//End Class Env
+
+struct Field{
+    
+    var text: String
+    var enabled: Bool
+    
+}
+
+
+struct ContentView: View {
+    
+ @EnvironmentObject var env: Env  //instead of @Binding we put @EnvironmentObject to do binding for the whole Env class
+    
     var body: some View {
         
         
@@ -49,39 +108,28 @@ struct ContentView: View {
             VStack{
              
                 
-                Text(winner).foregroundColor(.red).font(.system(size: 50, weight: .bold, design: .rounded)).background(Color.white)
+                Text(env.winner).foregroundColor(.red).font(.system(size: 50, weight: .bold, design: .rounded)).background(Color.white)
                 
-                Text("\(currentPlayer) turn").foregroundColor(.black).font(.system(size: 50, weight: .bold, design: .rounded)).background(Color.white).padding(10)
+                Text("\(env.currentPlayer) turn").foregroundColor(.black).font(.system(size: 50, weight: .bold, design: .rounded)).background(Color.white).padding(10)
                 
                   
-                    Grid(fields: $fields, currentPlayer: $currentPlayer, counter: $counter, winner: $winner)
+                Grid()
                     
                     
                    
                      
-                    if winner != ""{
-                        
-                        
-                        
+                    if env.winner != ""{
                         Button(action: {
-                              //resatrt
-                           
-                              
-                            self.restartGame()
-                              
-                              
+                              //restart
+                            self.env.restartGame()
                           }){
                               
-                            Text("Restart Game?").foregroundColor(.blue).font(.system(size: 40, design: .rounded)).shadow(radius: 12).background(Color.white).offset(y:20)
+                            Text("Restart Game?").foregroundColor(.blue).font(.system(size: 40, design: .rounded)).shadow(radius: 12).background(Color.white).offset(y:2)
                               
                           }
                         
                     }
-                        
-                        
-                    
-                    
-                    
+                       
             }//VStack
         }.edgesIgnoringSafeArea(.all)//ZStack
         
@@ -92,7 +140,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(winner: "")
+        ContentView().environmentObject(Env())
     }
 }
 
@@ -100,12 +148,7 @@ struct ContentView_Previews: PreviewProvider {
 
 struct Grid: View {
     
-    
-    
-    @Binding var fields : [[Field]]
-    @Binding var currentPlayer: String
-    @Binding var counter: Int
-    @Binding var winner: String
+    @EnvironmentObject var env: Env
     
     var body: some View {
         VStack(spacing: 20) {
@@ -114,7 +157,7 @@ struct Grid: View {
                 HStack(spacing: 20){
                     ForEach(0 ..< 3) { c in
                         
-                        XOButton(fields: self.$fields, currentPlayer: self.$currentPlayer, counter: self.$counter, winner: self.$winner, r: r, c: c)
+                        XOButton(r: r, c: c)
                     }//H forEach
                 }//HStack
             }//vertical forEach
@@ -127,73 +170,31 @@ struct Grid: View {
 
 struct XOButton: View {
     
-       @Binding var fields : [[Field]]
-       @Binding var currentPlayer: String
-       @Binding var counter: Int
-       @Binding var winner: String
+    @EnvironmentObject var env: Env
        var r: Int
         var c: Int
     var body: some View {
+        
         Button(action: {
             
-            
-            
-            if self.fields[self.r][self.c].enabled{
-                self.fields[self.r][self.c].text = self.currentPlayer
-                self.currentPlayer = self.currentPlayer == "X" ? "O" : "X"
-                self.fields[self.r][self.c].enabled = false
-                
-                self.counter += 1
-                self.checkWinners()
-            }
+            self.env.buttonAction(r: self.r,c: self.c)
+           
+        
             
         }){
             
             
-            Text(self.fields[r][c].text).frame(width: 100, height: 100).foregroundColor(.white).font(.system(size: 50, weight: .bold, design: .rounded)).background(Color.blue).shadow(radius: 12)
+            Text(self.env.fields[r][c].text).frame(width: 100, height: 100).foregroundColor(.white).font(.system(size: 50, weight: .bold, design: .rounded)).background(Color.blue).shadow(radius: 12)
             
             
         }
     }
     
     
-    func checkWinners(){
-            
-            
-            func checkWinner(p: String){
-            
-            let r1 = fields[0][0].text == p && fields[0][1].text == p && fields[0][2].text == p
-            let r2 = fields[1][0].text == p && fields[1][1].text == p && fields[1][2].text == p
-            let r3 = fields[2][0].text == p && fields[2][1].text == p && fields[2][2].text == p
-            
-            
-            let c1 = fields[0][0].text == p && fields[1][0].text == p && fields[2][0].text == p
-            let c2 = fields[0][1].text == p && fields[1][1].text == p && fields[2][1].text == p
-            let c3 = fields[0][2].text == p && fields[1][2].text == p && fields[2][2].text == p
-                
-            let d1 = fields[0][0].text == p && fields[1][1].text == p && fields[2][2].text == p
-            let d2 = fields[0][2].text == p && fields[1][1].text == p && fields[2][0].text == p
-            
-            
-            if r1 || r2 || r3 || c1 || c2 || c3 || d1 || d2{
-                
-                winner = "\(p) wins!!"
-                
-                
-            }
-            else if counter == 9 && winner == ""{
-                
-                winner = "Tie!!"
-                
-                }
-                
-            }//checkWinner
-            checkWinner(p: "X")
-            checkWinner(p: "O")
-            
-            
-        }//checkWinners
+   
         
         
     }
 }
+
+
